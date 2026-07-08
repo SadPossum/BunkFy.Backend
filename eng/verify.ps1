d72d5e40dd665ecc75a3499447b541f0f3c68b67
SadPossum <aprokudanov@gmail.com>
2026-07-08 17:59:10 +0300

    Fix backend verify script strict mode

--- eng/verify.ps1
+++ eng/verify.ps1
@@ -6,6 +6,3 @@ param(
 . (Join-Path $PSScriptRoot 'common.ps1')
 
 & (Join-Path $PSScriptRoot 'gma-validate.ps1') -SkipRestore:$SkipRestore -SkipBuild:$SkipBuild
-if ($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
-}
